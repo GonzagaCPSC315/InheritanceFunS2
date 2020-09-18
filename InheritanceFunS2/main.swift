d1a0bfeb7bf72e91732341c2cdfd3a7f1530457d
Gina Sprint <sprint@gonzaga.edu>
2020-09-18 08:21:14 -0700

    Initial Commit

---
+++ InheritanceFunS2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  InheritanceFunS2
+//
+//  Created by Gina Sprint on 9/18/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
